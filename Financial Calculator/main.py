d0cb85fc7c4abe1154625eef422a8dc7e35b0bb6
Ucas-DanielB <daniel.blanco@ucas-edu.net>
2025-01-08 10:48:59 -0700

    Financial Calculator

---
+++ Financial Calculator/main.py
@@ -0,0 +1,15 @@
+#OVERVIEW:
+#Create a program that completes the following basic financial calculations:
+
+#How long it will take to save for a goal based on a weekly or monthly deposit
+#Compound Interest Calculator 
+#Budget Allocator (use set percentages to divide an income into spending categories like savings, entertainment, and food)
+#Sale Price Calculator (apply discounts to prices)
+#Tip Calculator
+
+#PROJECT STEPS:
+#Create a main function that will run the user interface
+#Using project decomposition to figure out what other functions you need and how they interact with each other
+#Create your functions
+#Have at least 2 people test your code to make sure it works
+
