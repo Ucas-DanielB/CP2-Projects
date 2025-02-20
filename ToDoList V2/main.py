4d24b843e80f2925f073943efb5332fcb4fbf9a6
Ucas-DanielB <daniel.blanco@ucas-edu.net>
2025-02-20 10:34:00 -0700

    changed ToDoList V2

--- ToDoList V2/main.py
+++ ToDoList V2/main.py
@@ -7,4 +7,26 @@
 #Add items to the to do list
 #Mark item as complete
 #Delete item from to do list
-            
+
+#r = read (won’t create file)
+#w = write (won’t create file)
+#w+ = read and write
+#a = append (add things but not write over) (Will create the file if it isn’t there)
+#x = create a file
+
+#example: 
+
+#with open(‘Notes_2nd/things.txt’, “w”) as file:
+	#File.write(“\nI just made another line on my file!”)
+
+#with open(‘Notes_2nd/things.txt’, “r”) as file:
+	#print(file.read())
+
+#Read and write you do the same but just have both of them in the same space of code.
+
+#File.write(“”) as file:
+#print(file.read())
+
+def add_items():
+    with open("ToDoList V2/ToDoList.txt", "w") as file:
+        file.write("\nLook ma Im doing it")
