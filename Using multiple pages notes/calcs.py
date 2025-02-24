0f11bf881ca156246bb406571fe8892c6efd0d00
Ucas-DanielB <daniel.blanco@ucas-edu.net>
2025-02-24 10:31:45 -0700

    Notes

---
+++ Using multiple pages notes/calcs.py
@@ -0,0 +1,13 @@
+#Daniel Blanco, math calculations
+
+def add(num_one, num_two):
+    return (num_one+num_two)
+
+def sub(num_one, num_two):
+    return (num_one-num_two)
+
+def div(num_one, num_two):
+    return (num_one/num_two)
+
+def mult(num_one, num_two):
+    return (num_one*num_two)
---
+++ Using multiple pages notes/main.py
@@ -0,0 +1,11 @@
+# Daniel Blanco
+
+from calcs import add as x
+
+print(x(4,6))
+
+
+def info():
+    return "Balake", 15, "black"
+
+name, age, color = info()
