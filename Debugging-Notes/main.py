68d2738f2d76f6525389bd334a5b413bd422cb9f
Ucas-DanielB <daniel.blanco@ucas-edu.net>
2025-03-25 10:07:32 -0600

    Debugging Notes

--- Debugging-Notes/main.py
+++ Debugging-Notes/main.py
@@ -39,13 +39,24 @@ exception - when there is an exception raised
 #What are some ways we can debug by tracing?
     # - So we can make a function that checks the specifics for us.
 #How do you access the debugger in VS Code?
-    # - By pressing F5 go to top right, click arrow, and select python debugger: debug python file
+    # - By pressing F5 go to top right, click arrow, and select python debugger: debug python file.
 #What is testing?
-    # - 
+    # - Going through the code trying to break it, and having testers that are not the programmers.
 #What are boundary conditions?
-    # - 
+    # - User conditions that are strange and/or likely to cause issues.
+age = 18
+if age >= 18:
+    print("you can vote")
+elif age >= 16:
+    print("You can drive")
+elif age == 15:
+    print("You can get your learners permit")
+elif age >= 5:
+    print("you can go to school")
+else:
+    print("You're too young for anything")
 #How do you handle when users give strange inputs?
-    # - 
+    # - After printing they have inputted something wrong you loop back so they can input correctly.
 
 def sub(numone, numetwo):
     print( numone-numetwo)
