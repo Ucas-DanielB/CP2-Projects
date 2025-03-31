93cd263c31c33157fb6a583f1bf14e86e3b9fb52
Ucas-DanielB <daniel.blanco@ucas-edu.net>
2025-03-31 07:50:08 -0600

    Update coin_change.py

--- Coin Change Problem/coin_change.py
+++ Coin Change Problem/coin_change.py
@@ -1,11 +1,10 @@
 def get_min_coins(target, coins):
-    #Finds the minimum number of coins to make the target amount.
+    """Finds the minimum number of coins to make the target amount."""
     coins.sort(key=lambda x: x[1], reverse=True)  # Sort coins by value (descending)
     
     coin_count = {}  # Dictionary to store coin breakdown
     total_coins = 0
     
-    # Calculations for coins to make the target amount
     for name, value in coins:
         if target >= value:
             count = target // value
@@ -13,9 +12,7 @@ def get_min_coins(target, coins):
             coin_count[name] = count
             total_coins += count
 
-
     if target > 0:
         return None, None  # Change cannot be made with given denominations
-
 
     return total_coins, coin_count
