e8f8320b4a51c3d76861967ce5b668b52d5cf023
Ucas-DanielB <daniel.blanco@ucas-edu.net>
2025-04-10 10:22:20 -0600

    Made personal portfolio

---
+++ personal_portfolio/main.py
@@ -0,0 +1,4 @@
+# Daniel Blanco, Personal Portfolio
+
+print('''Welcome to my personal portfolio, this program showcases all of my projects that I am most proud of 
+This program will in many ways serve as a programming resume for you so please make sure to make the user interfaces VERY intuitive.''')
