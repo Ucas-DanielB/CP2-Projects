df9227b19c6fa4abed5dad173416bec202486194
Ucas-DanielB <daniel.blanco@ucas-edu.net>
2025-04-10 10:48:19 -0600

    update main UI

--- personal_portfolio/main.py
+++ personal_portfolio/main.py
@@ -1,17 +1,18 @@
 # Daniel Blanco, Personal Portfolio
 
-# from InquirerPy import prompt
-# 
-# questions = [
-#     {"type": "input", "message": "What's your name:", "name": "name"},
-#     {
-#         "type": "list",
-#         "message": "What's your favourite programming language:",
-#         "choices": ["Go", "Python", "Rust", "JavaScript"],
-#     },
-#     {"type": "confirm", "message": "Confirm?"},
-# ]
-# result = prompt(questions)
-# name = result["name"]
-# fav_lang = result[1]
-# confirm = result[2]
+from InquirerPy import prompt
+
+questions = [
+    {"type": "input", "message": "What's your name:", "name": "name"},
+    {
+        "type": "list",
+        "message": "What's your favourite programming language:",
+        "choices": ["Go", "Python", "Rust", "JavaScript"],
+    },
+    {"type": "confirm", "message": "Confirm?"},
+]
+result = prompt(questions)
+name = result["name"]
+fav_lang = result[1]
+confirm = result[2]
+
