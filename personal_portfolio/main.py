6bdc5cf1c837061b03a0a2e7348a1769a52e5232
Ucas-DanielB <daniel.blanco@ucas-edu.net>
2025-04-11 10:50:08 -0600

    Update main.py

--- personal_portfolio/main.py
+++ personal_portfolio/main.py
@@ -34,12 +34,12 @@ def main():
         return
 
     descriptions = {
-        "1. Personal Library (personal_P-library-MG.py)": "NOTE",
-        "2. RPG Battle System (personal_battle_S.py)": "NOTE",
-        "3. Finance Calculator (personal_finance-calc.py)": "NOTE",
-        "4. Movie Recommender (personal_movie-recommender.py)": "NOTE",
-        "5. Tic Tac Toe (personal_tic-tac-toe.py)": "NOTE",
-        "6. To-Do List (personal_to-do-list V2.py)": "NOTE"
+        "1. Personal Library (personal_P_library_MG.py)": "The project allows you to add, display, search, or remove books. The process of programming it was kind of annoying, as I couldn't get it right the first few times. I learned about different list types and how to utilize them in a project. This was an individual project.",
+        "2. RPG Battle System (personal_battle_S.py)": "This project allowed the user to create characters and fight with them. It took me forever to finish the program, and proved to be one of the hardest programs to code I had done. I learned how to use dictionaries. This was an individual project.",
+        "3. Finance Calculator (personal_finance_calc.py)": "A calculator made to budget allocate, time to save, compound/interest, sale price, and tip. The program was relatively easy to make, just figuring out how the math worked. I learned the different math calculations on code. This was an individual project.",
+        "4. Movie Recommender (personal_movie_recommender.py)": "This project viewed movie recommendations and could filter to find something specific. This was a hard program to code, as getting the code right was tedious. I learned to use different files with information on them. This was an individual project.",
+        "5. Tic Tac Toe (personal_tic_tac_toe.py)": "A Tic Tac Toe game. This i was one of the first hard projects to code, as I didn't quite understand the specifics to get right. I learned how to make my first visual, even if it used keyboard keys: |, x, o. This was an individual project.",
+        "6. To-Do List (personal_to_do_list.py)": "A program that kept a list even after closing the terminal on a different file. It was new material, so it took me awhile to get the hang of txt files. I learned how to actually use txt files. This was an individual project."
     }
 
     print("\n--- Project Description ---")
